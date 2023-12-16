c26c83013cc2b036217bc51000674f96ba06b01f
Varun <varunguleria1700@gmail.com>
2023-12-16 17:28:01 +0530

    backend-changes

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,13 @@
 from flask import Flask
-from flask.helpers import send_from_directory
-from flask_cors import CORS,cross_origin
-app=Flask(__name__,static_folder='frontend/build')
+from flask_cors import CORS
+app=Flask(__name__)
 CORS(app)
 @app.route('/route')
-@cross_origin()
 def index():
     return {
         "tot":"run suc"
     }
 @app.route('/')
-@cross_origin
 def serve():
     return {"name":"Naam"};
 
